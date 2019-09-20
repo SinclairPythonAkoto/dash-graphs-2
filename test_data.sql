10dbf7a0fd8f0ba6d50be9cd19a9401a1df63983
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-09-20 10:51:02 +0100

    file upload - test_data.sql

---
+++ test_data.sql
@@ -0,0 +1,40 @@
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Sinclair', 'no', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Shalieka', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Derek', 'yes', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Jeff', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Daniella', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Sinead', 'yes', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Cynthia', 'no', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Sandra', 'yes', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Stella', 'no', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Chris', 'no', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Michael', 'yes', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Kofi', 'yes', 'no');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Leslie', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Edna', 'no', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Vivian', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Angela', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Kofi', 'no', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Wayne', 'yes', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Chris', 'no', 'yes');
+INSERT INTO dash_graphs (name, first_visit, visit_again)
+VALUES ('Danny', 'yes', 'no');
